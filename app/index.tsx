6bbfd7af46ae35d8f0f6b61ba592a5fc21956c5b
Rishisinghparihar <rsparihar876@gmail.com>
2025-03-25 19:40:00 +0530

    remove unused components and hooks; add tailwind configuration

---
+++ app/index.tsx
@@ -0,0 +1,43 @@
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+  Easing,
+} from 'react-native-reanimated';
+import { View, Button, StyleSheet, Text } from 'react-native';
+// import { Text } from 'react-native-reanimated/lib/typescript/Animated';
+
+export default function AnimatedStyleUpdateExample() {
+  const randomWidth = useSharedValue(10);
+
+  const config = {
+    duration: 500,
+    easing: Easing.bezier(0.5, 0.01, 0, 1),
+  };
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: withTiming(randomWidth.value, config),
+    };
+  });
+
+  return (
+    <View style={styles.container}>
+  <Text>hello world...!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: 100,
+    height: 80,
+    backgroundColor: 'black',
+    margin: 30,
+  },
+});
